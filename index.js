eb6efdb268159e27a57e3d88334adc53a8bfa0d8
Abhijit Gayen <109941089+AbhijitCogoport@users.noreply.github.com>
2022-08-20 01:12:40 +0530

    add mistake

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.post("/webhook",(req,res)=>{ //i want some
 
     let body_param=req.body;
 
-    console.log(JSON.stringify(body_param,null,2));
+    console.log("just for working " ,JSON.stringify(body_param,null,2));
 
     if(body_param.object){
         console.log("inside body param");
