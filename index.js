6c02e42e8e87ea53874f79342e74c3dbca401fc2
Abhijit Gayen <109941089+AbhijitCogoport@users.noreply.github.com>
2022-08-20 02:19:38 +0530

    reverse  setup

--- index.js
+++ index.js
@@ -1,32 +1,16 @@
 const express=require("express");
 const body_parser=require("body-parser");
 const axios=require("axios");
-const WhatsappCloudAPI = require('whatsappcloudapi_wrapper');
-
 require('dotenv').config();
+
 const app=express().use(body_parser.json());
 
 const token=process.env.TOKEN;
 const mytoken=process.env.MYTOKEN;//prasath_token
 
-const Whatsapp = new WhatsappCloudAPI({
-    accessToken: process.env.Meta_WA_accessToken,
-    senderPhoneNumberId: process.env.Meta_WA_SenderPhoneNumberId,
-    WABA_ID: process.env.Meta_WA_wabaId,
-});
-
 app.listen(process.env.PORT,()=>{
     console.log("webhook is listening");
 });
-
-
-
-const EcommerceStore = require('./../utils/ecommerce_store.js');
-let Store = new EcommerceStore();
-const CustomerSession = new Map();
-
-
-
 
 //to verify the callback url from dashboard side - cloud api side
 app.get("/webhook",(req,res)=>{
@@ -48,319 +32,48 @@ app.get("/webhook",(req,res)=>{
 });
 
 app.post("/webhook",(req,res)=>{ //i want some 
-    console.log('POST: Someone is pinging me!');
-    try {
-        let data = Whatsapp.parseMessage(req.body);
 
-        if (data?.isMessage) {
-            let incomingMessage = data.message;
-            let recipientPhone = incomingMessage.from.phone; // extract the phone number of sender
-            let recipientName = incomingMessage.from.name;
-            let typeOfMsg = incomingMessage.type; // extract the type of message (some are text, others are images, others are responses to buttons etc...)
-            let message_id = incomingMessage.message_id; // extract the message id
+    let body_param=req.body;
 
-            // Start of cart logic
-            if (!CustomerSession.get(recipientPhone)) {
-                CustomerSession.set(recipientPhone, {
-                    cart: [],
-                });
+    console.log("out side working " ,JSON.stringify(body_param,null,2));
+
+    if(body_param?.object){
+        console.log("inside body param");
+        if(body_param?.entry && 
+            body_param.entry[0].changes && 
+            body_param.entry[0].changes[0].value.messages[0]  
+            ){
+                console.log('deep layer')
+               let phon_no_id=body_param.entry[0].changes[0].value.metadata.phone_number_id;
+               let from = body_param.entry[0].changes[0].value.messages[0].from; 
+               let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
+
+               console.log("phone number "+phon_no_id);
+               console.log("from "+from);
+               console.log("boady param "+msg_body);
+               let setall=phon_no_id+from;
+
+               axios({
+                   method:"POST",
+                   url:"https://graph.facebook.com/v13.0/"+phon_no_id+"/messages?access_token="+token,
+                   data:{
+                       messaging_product:"whatsapp",
+                       to:from,
+                       text:{
+                           body:"Hi.. I'm Prasath, your message is "+msg_body+setall
+                       }
+                   },
+                   headers:{
+                       "Content-Type":"application/json"
+                   }
+
+               });
+
+               res.sendStatus(200);
+            }else{
+                res.sendStatus(404);
             }
 
-            let addToCart = async ({ product_id, recipientPhone }) => {
-                let product = await Store.getProductById(product_id);
-                if (product.status === 'success') {
-                    CustomerSession.get(recipientPhone).cart.push(product.data);
-                }
-            };
-
-            let listOfItemsInCart = ({ recipientPhone }) => {
-                let total = 0;
-                let products = CustomerSession.get(recipientPhone).cart;
-                total = products.reduce(
-                    (acc, product) => acc + product.price,
-                    total
-                );
-                let count = products.length;
-                return { total, products, count };
-            };
-
-            let clearCart = ({ recipientPhone }) => {
-                CustomerSession.get(recipientPhone).cart = [];
-            };
-            // End of cart logic
-
-            if (typeOfMsg === 'text_message') {
-                await Whatsapp.sendSimpleButtons({
-                    message: `Hey ${recipientName}, \nGreetings from Cogoport Private Limited.\nHow can we help you?`,
-                    recipientPhone: recipientPhone,
-                    listOfButtons: [
-                        {
-                            title: 'Track Shipments',
-                            id: 'see_categories',
-                        },
-                        {
-                            title: 'Speak to a human',
-                            id: 'speak_to_human',
-                        },
-                    ],
-                });
-            }
-
-            if (typeOfMsg === 'radio_button_message') {
-                let selectionId = incomingMessage.list_reply.id;
-
-                if (selectionId.startsWith('product_')) {
-                    let product_id = selectionId.split('_')[1];
-                    let product = await Store.getProductById(product_id);
-                    const {
-                        price,
-                        title,
-                        description,
-                        category,
-                        image: imageUrl,
-                        rating,
-                    } = product.data;
-
-                    let emojiRating = (rvalue) => {
-                        rvalue = Math.floor(rvalue || 0); // generate as many star emojis as whole ratings
-                        let output = [];
-                        for (var i = 0; i < rvalue; i++) output.push('⭐');
-                        return output.length ? output.join('') : 'N/A';
-                    };
-
-                    let text = `_Title_: *${title.trim()}*\n\n\n`;
-                    text += `_Description_: ${description.trim()}\n\n\n`;
-                    text += `_Price_: $${price}\n`;
-                    text += `_Category_: ${category}\n`;
-                    text += `${
-                        rating?.count || 0
-                    } shoppers liked this product.\n`;
-                    text += `_Rated_: ${emojiRating(rating?.rate)}\n`;
-
-                    await Whatsapp.sendImage({
-                        recipientPhone,
-                        url: imageUrl,
-                        caption: text,
-                    });
-
-                    await Whatsapp.sendSimpleButtons({
-                        message: `Here is the product, what do you want to do next?`,
-                        recipientPhone: recipientPhone,
-                        message_id,
-                        listOfButtons: [
-                            {
-                                title: 'Add to cart🛒',
-                                id: `add_to_cart_${product_id}`,
-                            },
-                            {
-                                title: 'Speak to a human',
-                                id: 'speak_to_human',
-                            },
-                            {
-                                title: 'See more products',
-                                id: 'see_categories',
-                            },
-                        ],
-                    });
-                }
-            }
-
-            if (typeOfMsg === 'simple_button_message') {
-                let button_id = incomingMessage.button_reply.id;
-
-                if (button_id === 'speak_to_human') {
-                    // respond with a list of human resources
-                    await Whatsapp.sendText({
-                        recipientPhone: recipientPhone,
-                        message: `Not to brag, but unlike humans, chatbots are super fast⚡, we never sleep, never rest, never take lunch🍽 and can multitask.\n\nAnway don't fret, a hoooooman will 📞contact you soon.\n\nWanna blast☎ his/her phone😈?\nHere are the contact details:`,
-                    });
-
-                    await Whatsapp.sendContact({
-                        recipientPhone: recipientPhone,
-                        contact_profile: {
-                            addresses: [
-                                {
-                                    city: 'Mumbai',
-                                    country: 'India',
-                                },
-                            ],
-                            name: {
-                                first_name: 'Sagar',
-                                last_name: 'Bankar',
-                            },
-                            org: {
-                                company: 'Cogoport Private Limited',
-                            },
-                            phones: [
-                                {
-                                    phone: '+91-9921218632',
-                                },
-                                {
-                                    phone: '+91-9921218632',
-                                },
-                            ],
-                        },
-                    });
-                }
-                if (button_id === 'see_categories') {
-                    let categories = await Store.getAllCategories();
-
-                    await Whatsapp.sendSimpleButtons({
-                        message: `We have several categories.\nChoose one of them.`,
-                        recipientPhone: recipientPhone,
-                        message_id,
-                        listOfButtons: categories.data
-                            .slice(0, 3)
-                            .map((category) => ({
-                                title: category,
-                                id: `category_${category}`,
-                            })),
-                    });
-                }
-
-                if (button_id.startsWith('category_')) {
-                    let selectedCategory = button_id.split('category_')[1];
-                    let listOfProducts = await Store.getProductsInCategory(
-                        selectedCategory
-                    );
-
-                    let listOfSections = [
-                        {
-                            title: `🏆 Top 3: ${selectedCategory}`.substring(
-                                0,
-                                24
-                            ),
-                            rows: listOfProducts.data
-                                .map((product) => {
-                                    let id = `product_${product.id}`.substring(
-                                        0,
-                                        256
-                                    );
-                                    let title = product.title.substring(0, 21);
-                                    let description =
-                                        `${product.price}\n${product.description}`.substring(
-                                            0,
-                                            68
-                                        );
-
-                                    return {
-                                        id,
-                                        title: `${title}...`,
-                                        description: `$${description}...`,
-                                    };
-                                })
-                                .slice(0, 10),
-                        },
-                    ];
-
-                    await Whatsapp.sendRadioButtons({
-                        recipientPhone: recipientPhone,
-                        headerText: `#BlackFriday Offers: ${selectedCategory}`,
-                        bodyText: `Our Santa 🎅🏿 has lined up some great products for you based on your previous shopping history.\n\nPlease select one of the products below:`,
-                        footerText: 'Powered by: BMI LLC',
-                        listOfSections,
-                    });
-                }
-
-                if (button_id.startsWith('add_to_cart_')) {
-                    let product_id = button_id.split('add_to_cart_')[1];
-                    await addToCart({ recipientPhone, product_id });
-                    let numberOfItemsInCart = listOfItemsInCart({
-                        recipientPhone,
-                    }).count;
-
-                    await Whatsapp.sendSimpleButtons({
-                        message: `Your cart has been updated.\nNumber of items in cart: ${numberOfItemsInCart}.\n\nWhat do you want to do next?`,
-                        recipientPhone: recipientPhone,
-                        message_id,
-                        listOfButtons: [
-                            {
-                                title: 'Checkout 🛍️',
-                                id: `checkout`,
-                            },
-                            {
-                                title: 'See more products',
-                                id: 'see_categories',
-                            },
-                        ],
-                    });
-                }
-
-                if (button_id === 'checkout') {
-                    let finalBill = listOfItemsInCart({ recipientPhone });
-                    let invoiceText = `List of items in your cart:\n`;
-
-                    finalBill.products.forEach((item, index) => {
-                        let serial = index + 1;
-                        invoiceText += `\n#${serial}: ${item.title} @ $${item.price}`;
-                    });
-
-                    invoiceText += `\n\nTotal: $${finalBill.total}`;
-
-                    Store.generatePDFInvoice({
-                        order_details: invoiceText,
-                        file_path: `./invoice_${recipientName}.pdf`,
-                    });
-
-                    await Whatsapp.sendText({
-                        message: invoiceText,
-                        recipientPhone: recipientPhone,
-                    });
-
-                    await Whatsapp.sendSimpleButtons({
-                        recipientPhone: recipientPhone,
-                        message: `Thank you for shopping with us, ${recipientName}.\n\nYour order has been received & will be processed shortly.`,
-                        message_id,
-                        listOfButtons: [
-                            {
-                                title: 'See more products',
-                                id: 'see_categories',
-                            },
-                            {
-                                title: 'Print my invoice',
-                                id: 'print_invoice',
-                            },
-                        ],
-                    });
-
-                    clearCart({ recipientPhone });
-                }
-
-                if (button_id === 'print_invoice') {
-                    // Send the PDF invoice
-                    await Whatsapp.sendDocument({
-                        recipientPhone,
-                        caption: `Mom-N-Pop Shop invoice #${recipientName}`,
-                        file_path: `./invoice_${recipientName}.pdf`,
-                    });
-
-                    // Send the location of our pickup station to the customer, so they can come and pick their order
-                    let warehouse = Store.generateRandomGeoLocation();
-
-                    await Whatsapp.sendText({
-                        recipientPhone: recipientPhone,
-                        message: `Your order has been fulfilled. Come and pick it up, as you pay, here:`,
-                    });
-
-                    await Whatsapp.sendLocation({
-                        recipientPhone,
-                        latitude: warehouse.latitude,
-                        longitude: warehouse.longitude,
-                        address: warehouse.address,
-                        name: 'Mom-N-Pop Shop',
-                    });
-                }
-            }
-
-            await Whatsapp.markMessageAsRead({
-                message_id,
-            });
-        }
-
-        return res.sendStatus(200);
-    } catch (error) {
-        console.error({ error });
-        return res.sendStatus(500);
     }
 
 });
