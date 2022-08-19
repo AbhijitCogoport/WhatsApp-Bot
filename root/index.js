2678e6bec458890b9504ebd06183c445ca8d9bb4
Abhijit Gayen <109941089+AbhijitCogoport@users.noreply.github.com>
2022-08-20 02:11:39 +0530

    new setup

--- root/index.js
+++ root/index.js
@@ -1,41 +1,41 @@
 'use strict';
 const router = require('express').Router();
 
-// const WhatsappCloudAPI = require('whatsappcloudapi_wrapper');
+const WhatsappCloudAPI = require('whatsappcloudapi_wrapper');
 
-// const Whatsapp = new WhatsappCloudAPI({
-//     accessToken: process.env.Meta_WA_accessToken,
-//     senderPhoneNumberId: process.env.Meta_WA_SenderPhoneNumberId,
-//     WABA_ID: process.env.Meta_WA_wabaId,
-// });
+const Whatsapp = new WhatsappCloudAPI({
+    accessToken: process.env.Meta_WA_accessToken,
+    senderPhoneNumberId: process.env.Meta_WA_SenderPhoneNumberId,
+    WABA_ID: process.env.Meta_WA_wabaId,
+});
 
 const EcommerceStore = require('./../utils/ecommerce_store.js');
 let Store = new EcommerceStore();
 const CustomerSession = new Map();
 
-// router.get('/meta_wa_callbackurl', (req, res) => {
-//     try {
-//         console.log('GET: Someone is pinging me!');
+router.get('/meta_wa_callbackurl', (req, res) => {
+    try {
+        console.log('GET: Someone is pinging me!');
 
-//         let mode = req.query['hub.mode'];
-//         let token = req.query['hub.verify_token'];
-//         let challenge = req.query['hub.challenge'];
+        let mode = req.query['hub.mode'];
+        let token = req.query['hub.verify_token'];
+        let challenge = req.query['hub.challenge'];
 
-//         if (
-//             mode &&
-//             token &&
-//             mode === 'subscribe' &&
-//             process.env.Meta_WA_VerifyToken === token
-//         ) {
-//             return res.status(200).send(challenge);
-//         } else {
-//             return res.sendStatus(403);
-//         }
-//     } catch (error) {
-//         console.error({ error });
-//         return res.sendStatus(500);
-//     }
-// });
+        if (
+            mode &&
+            token &&
+            mode === 'subscribe' &&
+            process.env.Meta_WA_VerifyToken === token
+        ) {
+            return res.status(200).send(challenge);
+        } else {
+            return res.sendStatus(403);
+        }
+    } catch (error) {
+        console.error({ error });
+        return res.sendStatus(500);
+    }
+});
 
 router.post('/meta_wa_callbackurl', async (req, res) => {
     console.log('POST: Someone is pinging me!');
