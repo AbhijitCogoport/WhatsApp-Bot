34a7fb6c6b064b9b5567bfc6910092683af9104b
Abhijit Gayen <109941089+AbhijitCogoport@users.noreply.github.com>
2022-08-20 01:47:24 +0530

    add mistake

--- index.js
+++ index.js
@@ -37,13 +37,14 @@ app.post("/webhook",(req,res)=>{ //i want some
 
     console.log("just for working " ,JSON.stringify(body_param,null,2));
 
-    if(body_param.object){
+    if(body_param?.object){
         console.log("inside body param");
-        if(body_param.entry && 
+        if(body_param?.entry && 
             body_param.entry[0].changes && 
             body_param.entry[0].changes[0].value.message && 
             body_param.entry[0].changes[0].value.messages[0]  
             ){
+                console.log('deep layer')
                let phon_no_id=body_param.entry[0].changes[0].value.metadata.phone_number_id;
                let from = body_param.entry[0].changes[0].value.messages[0].from; 
                let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
