ff4479ee3fa210d64a1739b8f6fc425cb5f4255c
Abhijit Gayen <109941089+AbhijitCogoport@users.noreply.github.com>
2022-08-20 21:20:54 +0530

    this is setup

--- index.js
+++ index.js
@@ -132,10 +132,54 @@ app.post("/webhook", (req, res) => {
                         context: {
                             "message_id": msg_id
                         },
-                        type: "text",
-                        "text": {
-                            "preview_url": false,
-                            "body": "i am not able to understand fack . " + msg_id
+                        type: "interactive",
+                        "interactive": {
+                            "type": "list",
+                            "header": {
+                                "type": "text",
+                                "text": "<HEADER_TEXT>"
+                            },
+                            "body": {
+                                "text": "<BODY_TEXT>"
+                            },
+                            "footer": {
+                                "text": "<FOOTER_TEXT>"
+                            },
+                            "action": {
+                                "button": "<BUTTON_TEXT>",
+                                "sections": [
+                                    {
+                                        "title": "<LIST_SECTION_1_TITLE>",
+                                        "rows": [
+                                            {
+                                                "id": "<LIST_SECTION_1_ROW_1_ID>",
+                                                "title": "<SECTION_1_ROW_1_TITLE>",
+                                                "description": "<SECTION_1_ROW_1_DESC>"
+                                            },
+                                            {
+                                                "id": "<LIST_SECTION_1_ROW_2_ID>",
+                                                "title": "<SECTION_1_ROW_2_TITLE>",
+                                                "description": "<SECTION_1_ROW_2_DESC>"
+                                            }
+                                        ]
+                                    },
+                                    {
+                                        "title": "<LIST_SECTION_2_TITLE>",
+                                        "rows": [
+                                            {
+                                                "id": "<LIST_SECTION_2_ROW_1_ID>",
+                                                "title": "<SECTION_2_ROW_1_TITLE>",
+                                                "description": "<SECTION_2_ROW_1_DESC>"
+                                            },
+                                            {
+                                                "id": "<LIST_SECTION_2_ROW_2_ID>",
+                                                "title": "<SECTION_2_ROW_2_TITLE>",
+                                                "description": "<SECTION_2_ROW_2_DESC>"
+                                            }
+                                        ]
+                                    }
+                                ]
+                            }
                         }
                     },
                     headers: {
@@ -149,7 +193,11 @@ app.post("/webhook", (req, res) => {
                     url: "https://graph.facebook.com/v13.0/" +phon_no_id +"/messages?access_token=" +token,
                     data: {
                         messaging_product: "whatsapp",
+                        recipient_type: "individual",
                         to: from,
+                        context: {
+                            "message_id": msg_id
+                        },
                         type: "text",
                         "text": {
                             "preview_url": false,
