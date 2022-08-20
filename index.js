023a05ee6f42c0b16215e53c899367450166cb50
Abhijit Gayen <109941089+AbhijitCogoport@users.noreply.github.com>
2022-08-20 13:16:38 +0530

    small error

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ app.post("/webhook", (req, res) => {
         body_param.entry[0].changes[0].value.metadata.phone_number_id;
       let from = body_param.entry[0].changes[0].value.messages[0].from;
       let msg_body='none';
-      if(body_param.entry[0].changes[0].value.messages[0].text.body){
+      if(body_param.entry[0].changes[0].value.messages[0]){
          msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
       }
       
